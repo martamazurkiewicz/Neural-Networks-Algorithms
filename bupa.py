18aeb8a3d3346b2c74a6f0e34e4830226d7f5176
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2022-04-16 18:10:43 +0200

    Refactor bupa function
    Should now be more readable and reusable

--- bupa.py
+++ bupa.py
@@ -15,48 +15,57 @@ def calc_z_vector(misclassified_vectors):
 def calculate_new_weights(misclassified_vectors, old_weights_vector, ro):
     return old_weights_vector + ro * calc_z_vector(misclassified_vectors)
 
-### CONFIGURATION ###
-X = [[1,0,0], [1,0,1], [1,1,0], [1,1,1]]
-D_AND = [0, 0, 0, 1]
-D_XOR = [0, 1, 1, 0]
-D_OR = [0, 1, 1, 1]
-RO = 1
-ACTIVATION_FN = lambda x: 1 if x > 0 else 0
-INIT_WEIGHTS = np.array([0.5, 0, 1])
-X_VECT = np.array(X)
-MAX_ITERATIONS = 10000
-D = D_AND
-### END CONFIGURATION ###
-
-EXP_RES_VECT = np.array(D)
-misclassifications = 0
-iterations = 0
-weights_vector = INIT_WEIGHTS
-
-if not all(len(l) == len(X[0]) for l in iter(X)):
-    print("Vectors in training set X must be of same length")
-    exit(1)
-if(len(X) != len(D)):
-    print("Each vector in training set X must have a corresponding value in desired values vector D")
-    exit(2)
-
-while iterations < MAX_ITERATIONS:
-    weights_before_for_loop = weights_vector
+def bupa(train_set, teacher_set, init_weights, ro, activation_function, max_iterations = 10000):
+    if not all(len(l) == len(train_set[0]) for l in iter(train_set)):
+        raise Exception("Vectors in training set X must be of same length.")
+    if(len(train_set) != len(teacher_set)):
+        raise Exception("Each vector in training set must have a corresponding value in teacher set.")
+    if(not isinstance(max_iterations, int) or max_iterations <= 0):
+        raise Exception("Maximum number of iterations must be an integer greater than 0.")
+    if((not isinstance(ro, int) and not isinstance(ro, float)) or ro <= 0 or ro > 1):
+        raise Exception("The learning rate must be a number between 0(exclusive) and 1(inclusive)")
+    weights_vector = init_weights
     misclassifications = 0
-    misclassified_vectors = []
-    for i in range(len(X)):
-        fi = weighted_sum(X_VECT[i], weights_vector)
-        y = ACTIVATION_FN(fi)
-        if y!=EXP_RES_VECT[i]:
-            misclassified_vectors.append((EXP_RES_VECT[i] - y) * X_VECT[i])
-            misclassifications += 1
-    if len(misclassified_vectors) > 0:
-        weights_vector = calculate_new_weights(misclassified_vectors, weights_before_for_loop, RO)
-    if (weights_before_for_loop == weights_vector).all():
-        break
-    iterations += 1
+    iterations = 0
+    while iterations < max_iterations:
+        iterations += 1
+        misclassifications = 0
+        misclassified_vectors = []
+        for i in range(len(train_set)):
+            fi = weighted_sum(train_set[i], weights_vector)
+            y = activation_function(fi)
+            if y!=teacher_set[i]:
+                misclassified_vectors.append((teacher_set[i] - y) * train_set[i])
+                misclassifications += 1
+        if len(misclassified_vectors) > 0:
+            weights_vector = calculate_new_weights(misclassified_vectors, weights_vector, ro)
+        else:
+            break
+    if(misclassifications == 0):
+        return (True, weights_vector, iterations)
+    else:
+        return (False, None, iterations)
 
-if misclassifications == 0:
-    print(f"Final weights: {weights_vector}")
-else:
-    print("The algorithm did not converge")
+def main():
+    ### CONFIGURATION ###
+    X = [[1,0,0], [1,0,1], [1,1,0], [1,1,1]]
+    D_AND = [0, 0, 0, 1]
+    D_XOR = [0, 1, 1, 0]
+    D_OR = [0, 1, 1, 1]
+    RO = 1
+    ACTIVATION_FN = lambda x: 1 if x > 0 else 0
+    INIT_WEIGHTS = np.array([0.5, 0, 1])
+    X_VECT = np.array(X)
+    MAX_ITERATIONS = 10000
+    D = D_AND
+    EXP_RES_VECT = np.array(D)
+    ### END CONFIGURATION ###
+
+    has_converged, weights, iterations = bupa(X_VECT, EXP_RES_VECT, INIT_WEIGHTS, RO, ACTIVATION_FN)
+    if has_converged:
+        print(f"Final weights after {iterations} iterations: {weights}")
+    else:
+        print("The algorithm did not converge")
+
+if __name__ == "__main__":
+    main()
